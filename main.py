5376628a8cccfe4062f1407996d2ace138b155b1
for-the-zero <ourmicroid@outlook.com>
2023-10-04 23:20:30 +0800

    向前一小步，coding一大步（静态主要组件安好了

--- main.py
+++ main.py
@@ -3,14 +3,25 @@ import pyperclip as clip
 import pyautogui
 
 dpg.create_context()
-dpg.create_viewport(title='pincopy', width=600, height=300)
+dpg.create_viewport(title='pincopy', width=300, height=600)
 with dpg.font_registry():
-	default_font = dpg.add_font("./fonts/SourceHanSansSC-Normal.otf", 35)
+	with dpg.font("./fonts/SourceHanSansSC-Normal.otf", 30) as default_font:
+		dpg.add_font_range_hint(dpg.mvFontRangeHint_Default)
+		dpg.add_font_range_hint(dpg.mvFontRangeHint_Chinese_Full)
+
+
 
 with dpg.window(tag="Primary Window"):
-	dpg.add_text("Hello, world")
+	modep = dpg.add_slider_int(label="粘贴模式",tag="pmode",width=150,min_value=0,max_value=3,default_value=0)
+	copyb = dpg.add_button(label="获取剪贴板",tag="cbutton",width=150)
+	dpg.add_text("记录：")
 	...
 	dpg.bind_font(default_font)
+
+with dpg.tooltip('pmode'):
+	dpg.add_text("分别为：\n仅复制\n下次点击ctrl+v\n下次点击paste()\n下次点击输入")
+
+
 
 dpg.setup_dearpygui()
 dpg.show_viewport()
