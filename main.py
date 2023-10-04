fa7618aaaf61db0f17729bf65331305daaf317a4
for-the-zero <ourmicroid@outlook.com>
2023-10-04 15:03:58 +0800

    字体：OK

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
-import dearpygui as dpg
-import pyperclip as pc
+import dearpygui.dearpygui as dpg
+import pyperclip as clip
 import pyautogui
 
 dpg.create_context()
+dpg.create_viewport(title='pincopy', width=600, height=300)
+with dpg.font_registry():
+	default_font = dpg.add_font("./fonts/SourceHanSansSC-Normal.otf", 35)
+
 with dpg.window(tag="Primary Window"):
+	dpg.add_text("Hello, world")
 	...
-dpg.create_viewport(title='pincopy', width=400, height=400)
+	dpg.bind_font(default_font)
+
 dpg.setup_dearpygui()
 dpg.show_viewport()
 dpg.set_primary_window("Primary Window", True)
